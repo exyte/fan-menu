82cb21068eaec62d91a695610100f55765864986
Igor Zapletnev <igor.zapletnev@gmail.com>
2017-04-16 14:08:18 +0700

    Update settings

--- Circulo/FinanceViewController.swift
+++ Circulo/FinanceViewController.swift
@@ -65,7 +65,7 @@ class FinanceViewController: UIViewController, UITableViewDelegate, UITableViewD
         ]
 
         circleMenuView.distance = 90.0
-        circleMenuView.duration = 0.2
+        circleMenuView.duration = 0.35
         circleMenuView.halfMode = true
 
         circleMenuView.onButtonPressed = { button in
