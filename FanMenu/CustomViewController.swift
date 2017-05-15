62b4d8dd9768125cc7483029f088f32436223c33
Igor Zapletnev <igor.zapletnev@gmail.com>
2017-05-15 18:20:13 +0700

    Update customization example

--- FanMenu/CustomViewController.swift
+++ FanMenu/CustomViewController.swift
@@ -54,67 +54,79 @@ class CustomMenuScene {
     
     let node: Group
     
+    var animation: Animation?
+    var isOpen = false
+    
     init(customMenu: CustomMenu) {
         self.customMenu = customMenu
         
         menuButton = CustomMenuButtonScene(radius: customMenu.radius)
         buttonsGroup = CustomButtonsScene(customMenu: customMenu)
         
-        node = [menuButton.node, buttonsGroup.node].group()
+        node = [buttonsGroup.node, menuButton.node].group()
         
         menuButton.node.onTouchPressed { _ in
-            self.toggle()
+            if let animationValue = self.animation {
+                if animationValue.state() != .paused {
+                    return
+                }
+            }
+            
+            if self.isOpen {
+                self.close()
+            } else {
+                self.open()
+            }
         }
     }
     
-    var animation: Animation?
-    
     func close() {
         if let animationVal = self.animation {
-            animationVal.reverse().play()
-            self.animation = nil
-            return
+            self.animation = animationVal.reverse()
+            self.animation?.play()
         }
+        isOpen = false
     }
     
     func open() {
         let scale = customMenu.menuRadius / customMenu.radius
         self.animation = [
-            buttonsGroup.show(duration: customMenu.duration),
-            menuButton.open(duration: customMenu.duration, scale: scale)
+            buttonsGroup.show(duration: customMenu.duration, scale: scale),
+            menuButton.open(duration: customMenu.duration)
         ].combine()
         self.animation?.play()
-    }
-    
-    func toggle() {
-        if isOpen() {
-            close()
-        } else {
-            open()
-        }
-    }
-    
-    func isOpen() -> Bool {
-        return self.animation != nil
+        isOpen = true
     }
 }
 
 class CustomButtonsScene {
     
-    var node: Group!
-    let animationGroup: Group
+    let node: Group
+    let backgroundCircle: Shape
+    var animationGroup: Group!
+    var buttonsNode: Group!
+
+    let customMenu: CustomMenu
     
     init(customMenu: CustomMenu) {
-        animationGroup = Group()
-        node = Group()
+        self.customMenu = customMenu
         
-        node = Group(contents: customMenu.items.enumerated().map { (index, item) in
+        node = Group()
+        backgroundCircle = Shape(
+            form: Circle(r: customMenu.radius),
+            fill: Color.white.with(a: 0.5)
+        )
+        node.contents.append(backgroundCircle)
+        
+        buttonsNode = Group(contents: customMenu.items.enumerated().map { (index, item) in
             return self.createCustomButton(customMenu: customMenu, data: item, index: index)
         })
+        buttonsNode.opacity = 0.0
+        buttonsNode.place = Transform.identity.scale(sx: 0.4, sy: 0.4)
+        node.contents.append(buttonsNode)
         
-        node.contents.append(animationGroup)
-        node.place = Transform.identity.scale(sx: 0.4, sy: 0.4)
-        node.opacity = 0.0
+        animationGroup = Group()
+        buttonsNode.contents.append(animationGroup)
     }
     
     func createCustomButton(customMenu: CustomMenu, data: (String, String, Int), index: Int) -> Node {
@@ -156,15 +168,21 @@ class CustomButtonsScene {
         return [circle, image].group()
     }
     
-    func show(duration: Double) -> Animation {
+    func show(duration: Double, scale: Double) -> Animation {
         animationGroup.contents = []
         return [
-            node.opacityVar.animation(to: 1.0, during: duration),
-            node.placeVar.animation(to: node.place.scale(sx: 2.5, sy: 2.5), during: duration)
+            backgroundCircle.placeVar.animation(to: Transform.scale(sx: scale, sy: scale), during: duration),
+            backgroundCircle.opacityVar.animation(to: 0.0, during: duration),
+            buttonsNode.opacityVar.animation(to: 1.0, during: duration),
+            buttonsNode.placeVar.animation(to: buttonsNode.place.scale(sx: 2.5, sy: 2.5), during: duration)
         ].combine().easing(Easing.easeOut)
     }
     
     func select(node: Node, alpha: Double, color: Int, customMenu: CustomMenu) {
+        let index = self.buttonsNode.contents.index(of: node)
+        self.buttonsNode.contents.remove(at: index!)
+        self.buttonsNode.contents.append(node)
+        
         let contentAnimation = animationGroup.contentsVar.animation({ t in
             let shape = Shape(
                 form: Arc(
@@ -172,10 +190,11 @@ class CustomButtonsScene {
                     shift: alpha,
                     extent: 2 * Double.pi * t
                 ),
-                stroke: Stroke(fill: Color(val: color), width: customMenu.radius * 2 + 1)
+                stroke: Stroke(fill: Color(val: color), width: customMenu.radius * 2)
             )
             return [shape]
         }, during: customMenu.duration)
+        
         let opacityAnimation = node.opacityVar.animation(
             to: 1.0,
             during: customMenu.duration
@@ -185,37 +204,46 @@ class CustomButtonsScene {
         animation.play()
         
         contentAnimation.onComplete {
-            customMenu.close()
+            let closeAnimation = [
+                self.node.placeVar.animation(
+                    to: GeomUtils.centerScale(node: self.node, sx: 1.5, sy: 1.5),
+                    during: customMenu.duration
+                ),
+                self.node.opacityVar.animation(to: 0.0, during: customMenu.duration),
+                customMenu.scene!.menuButton.updateState(isOpen: false, duration: customMenu.duration)
+            ].combine()
+
+            closeAnimation.onComplete {
+                self.customMenu.updateNode()
+            }
+            closeAnimation.play()
+            
         }
     }
 }
 
 class CustomMenuButtonScene {
-    
-    var backgroundCircle: Shape!
+
     var openButtons: Group!
     var closeButtons: Group!
     var node: Group!
     
     init(radius: Double) {
-        backgroundCircle = Shape(
-            form: Circle(r: radius),
-            fill: Color.white.with(a: 0.5)
-        )
-        
         openButtons = createMenuState(image: "custom_share", radius: radius)
         closeButtons = createMenuState(image: "custom_close", radius: radius, a: 0.7)
         closeButtons.opacity = 0.0
         
-        node = [backgroundCircle, openButtons, closeButtons].group()
+        node = [openButtons, closeButtons].group()
     }
     
-    func open(duration: Double, scale: Double) -> Animation {
+    func open(duration: Double) -> Animation {
+        return updateState(isOpen: true, duration: duration)
+    }
+
+    func updateState(isOpen: Bool, duration: Double) -> Animation {
         return [
-            backgroundCircle.placeVar.animation(to: Transform.scale(sx: scale, sy: scale), during: duration),
-            backgroundCircle.opacityVar.animation(to: 0.0, during: duration),
-            openButtons.opacityVar.animation(to: 0.0, during: duration),
-            closeButtons.opacityVar.animation(to: 1.0, during: duration)
+            openButtons.opacityVar.animation(to: isOpen ? 0.0 : 1.0, during: duration),
+            closeButtons.opacityVar.animation(to: isOpen ? 1.0 : 0.0, during: duration)
         ].combine()
     }
     
