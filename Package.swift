8e04d2d057da5c0a97f881486485c78c39b3b2d9
shipinev <dmitry.shipinev+github@gmail.com>
2020-04-30 12:14:39 +0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+	name: "FanMenu",
+	platforms: [
+		.macOS(.v10_12),
+        .iOS(.v9)
+    ],
+    products: [
+    	.library(
+    		name: "FanMenu",
+    		targets: ["FanMenu"]
+    	)
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/exyte/Macaw",
+            .branch("master")
+        )
+    ],
+    targets: [
+    	.target(
+    		name: "FanMenu",
+            dependencies: ["Macaw"],
+            path: "Sources"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
